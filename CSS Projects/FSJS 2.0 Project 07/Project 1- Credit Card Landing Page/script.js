40c37639e733e924fe3c87ef6cf915285ea3a56d
Savinder <savindersingh500@gmail.com>
2022-12-17 23:09:42 +0530

    Responsiveness improved

--- CSS Projects/FSJS 2.0 Project 07/Project 1- Credit Card Landing Page/script.js
+++ CSS Projects/FSJS 2.0 Project 07/Project 1- Credit Card Landing Page/script.js
@@ -1,13 +1,13 @@
 /*******Adding Functionality to Hamburger button***************/
 const navbar = document.getElementsByTagName('nav')[0];
 const hamburgerBtn = document.getElementById("hamburger-icon");
-const crossBtn =document.getElementById("cross-btn");
-console.log(navbar,hamburgerBtn)
-hamburgerBtn.addEventListener("click",()=>{
+const crossBtn = document.getElementById("cross-btn");
+console.log(navbar, hamburgerBtn)
+hamburgerBtn.addEventListener("click", () => {
     navbar.style.display = "flex"
     hamburgerBtn.style.display = "none"
 })
-crossBtn.addEventListener("click",()=>{
+crossBtn.addEventListener("click", () => {
     navbar.style.display = "none"
     hamburgerBtn.style.display = "inline"
 })
