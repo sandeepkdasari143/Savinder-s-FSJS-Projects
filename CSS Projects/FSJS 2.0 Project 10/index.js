868586905e9a19c929b6b8709af4b6a8569cf237
Savinder <savindersingh500@gmail.com>
2022-12-31 22:49:08 +0530

    Hamburger icon logic improved

--- CSS Projects/FSJS 2.0 Project 10/index.js
+++ CSS Projects/FSJS 2.0 Project 10/index.js
@@ -1,23 +1,27 @@
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const crossBtn = document.getElementById("cross-btn");
-const listItems = document.getElementsByTagName("ul")[0];
-const navbar = document.getElementsByTagName("nav")[0];
-console.log(hamburgerBtn, crossBtn, listItems);
-hamburgerBtn.addEventListener("click", () => {
-    crossBtn.style.display = "inline";
-    listItems.style.left = "0%"
-    hamburgerBtn.style.display = "none";
-    navbar.style.backdropFilter = "blur(10px)";
-  
-    listItems.style.position = "relative"
-    listItems.style.transition = "all 0.5s ease-in-out"
-})
-crossBtn.addEventListener("click", () => {
-    crossBtn.style.display = "none";
-    listItems.style.left = "-110%"
-    hamburgerBtn.style.display = "inline";
-    navbar.style.backdropFilter = "blur(0)";
-   
-    listItems.style.position = "absolute";
-    listItems.style.transition = "none"
-})
+let hamburger = document.getElementsByClassName("hamburger");
+hamburger = Array.from(hamburger);
+
+for (let x of hamburger) {
+    x.addEventListener("click", () => {
+        for (let y of hamburger) {
+            y.classList.toggle("hamburger");
+            y.classList.toggle("cross");
+        }
+
+        hamburger.forEach((element, index) => {
+            console.log(element)
+            if (element.classList.contains("cross")) {
+                element.classList.add(`cross-${index + 1}`)
+                document.getElementsByClassName("list-items-conatiner")[0].style.left = "0%";
+                document.getElementsByClassName("list-items-conatiner")[0].style.position = "relative";
+                document.getElementsByTagName("nav")[0].style.backdropFilter = "blur(10px)"
+            } else {
+                element.classList.remove(`cross-${index + 1}`)
+                document.getElementsByClassName("list-items-conatiner")[0].style.left = "-110%";
+                document.getElementsByTagName("nav")[0].style.backdropFilter = "blur(0)";
+                document.getElementsByClassName("list-items-conatiner")[0].style.position = "absolute";
+            }
+        });
+
+    })
+}
