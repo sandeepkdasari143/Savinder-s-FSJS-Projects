cbfcc763de8b187f6baac95be9fb0ab91e9da1cd
Savinder <savindersingh500@gmail.com>
2023-01-05 11:50:49 +0530

    intial commit

--- Practice Work/JavaScript/Ternary Operator/index.js
+++ Practice Work/JavaScript/Ternary Operator/index.js
@@ -22,7 +22,7 @@ console.log(drink1);
 
  /* Q. Steven wants to build a very simple tip calculator for whenever he goes eating in a
 restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
-300. If the value is different, the tip is 20%.
+300. If the value is greater then the tip is 20%.
 Your tasks:
 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
 this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
