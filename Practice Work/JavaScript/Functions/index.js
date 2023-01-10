b93e9fe435154f308cd8100a46742e85c9b6705c
Savinder <savindersingh500@gmail.com>
2023-01-10 13:43:20 +0530

    Concept understood

---
+++ Practice Work/JavaScript/Functions/index.js
@@ -0,0 +1,34 @@
+//Functions are the heart of JavaScript.They are useful while build real industry projects with the help of JavaScript. Function is a piece of code which can be reuse again & again. It is similar to variables but variables can hold a value but function can hold chunks of code or many lines of complex code.
+
+//Syntax
+// first we have function key word, then name of the function , in pranthesis we give parameters, parameters are like labels or placeholders or varibles whose scope is limited to the function only and at last we have function body inside the curlt braces {}
+
+function greetingMessage() {
+    console.log(`Welcome ! JavaScript will change your life !`);
+}
+
+//If we won't call/run/invoke a function.It;s code will never executed by JavaScript Engine
+
+//Calling/Invoking/Running JS code ---> Now function code will execute
+greetingMessage();
+greetingMessage();
+greetingMessage();
+
+//As we mention above we can also provide input to the function and also can take output in the form of a value from function
+
+
+// function parameters like scoreTeamA & scoreTeamB can't be used outside of function body
+function WinnerTeam(scoreTeamA, scoreTeamB) {
+    console.log(`The score of Team A is ${scoreTeamA} & The score of Team B is ${scoreTeamB} `)
+    return scoreTeamA > scoreTeamB ? "Team A" : "Team B"
+}
+//WinnerTeam(25,23) // only print console.log() not the return value
+//Since this function is returing a value we need to store it somewhere
+
+const match_1 = WinnerTeam(25, 30) // 25 & 30 are called arguments and these will be replaced as a value to parameters inside the functions
+const match_2 = WinnerTeam(35, 30)
+console.log(`The winner of Match 1 is ${match_1}
+The winner of Match 2 is ${match_2}`)
+
+//Conclusion --> So functions help us to write clean & concise code based on DRY principle which Don't Repeat Yourself.
+//We have many inbuilt functions such as Number(), our favourite console.log() etc.
--- Practice Work/JavaScript/Strict mode/index.js
+++ Practice Work/JavaScript/Strict mode/index.js
@@ -6,7 +6,7 @@ const student = "John Mayor";
 let isCourseAccess = false;
 let isEnrolled = true;
 
-if(isEnrolled){
+if (isEnrolled) {
     isCourseAcess = true; // This will give error if strict mode is active else considered that new variable is declared 
     console.log(`Now ${student} has access to the course`)
 }
@@ -15,3 +15,5 @@ if(isEnrolled){
 
 const interface = "new UI";
 const private = "456";
+
+//Conclusion :- Strict mode restricts us from using some special keywords that are reserved for future releases & it limits us to use some weird features of JS and this limitation is important otherwise we may get many bugs in our code as our code becomes bigger & complex.
