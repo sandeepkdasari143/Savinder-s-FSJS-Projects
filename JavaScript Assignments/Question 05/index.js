af600eb421026d7073e52e60f655df6101038f98
Savinder <savindersingh500@gmail.com>
2023-01-12 23:45:51 +0530

    question solved

---
+++ JavaScript Assignments/Question 05/index.js
@@ -0,0 +1,7 @@
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+const str = "JavaScript is easy to learn. Even a child can also master it 😅😂"
+const arr = str.split(" ");  //We are splitting the string based on whitespace
+
+//output
+console.log(arr);
