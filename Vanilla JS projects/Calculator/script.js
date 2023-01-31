713408ea5812a47c29bf5d60ced5aa9ce6ad910c
Savinder <savindersingh500@gmail.com>
2023-02-01 03:05:28 +0530

    JS logic done

--- Vanilla JS projects/Calculator/script.js
+++ Vanilla JS projects/Calculator/script.js
@@ -1,55 +1,76 @@
 const screenInput = document.getElementsByClassName("screen")[0];
 // console.log(screen)
 const buttons = Array.from(document.getElementsByClassName("btn"));
-let n = 0;
+let isError = false;
+let isScreenEmpty = true;
+
 buttons.forEach((elem) => {
     elem.addEventListener("click", function (e) {
-        if (n === 1) {
-            screenInput.value = "";
-            n = 0;
-        }
+        if ([")", "%", "/", "x", "x²", "+"].includes(e.target.innerText)) {
+            if (!screenInput.value) {
+                screenInput.value = "";
+                console.log("1")
+            } else {
+                if (e.target.innerText === "x") {
+                    e.target.innerText = "*";
+                    screenInput.value += e.target.innerText;
+                    e.target.innerText = "x";
+                } else if (e.target.innerText === "x²") {
+                    console.log(e.target)
+                    e.target.innerText = "**2";
+                    screenInput.value += e.target.innerText;
+                    screenInput.value = eval(screenInput.value)
+                    e.target.innerHTML = `x²`
+                }
+                else {
+                    screenInput.value += e.target.innerText;
+                }
+            }
+        } else {
+            if (isError) {
+                screenInput.value = "";
+                isError = false;
+            }
+            if (e.target.innerText !== "=") {
+                if (e.target.innerText !== "x" && !isError && e.target.innerText !== 'x y') {
+                    screenInput.value += e.target.innerText;
+                }
+            }
+            switch (e.target.innerText) {
+                case "AC":
+                    screenInput.value = "";
+                    break;
+                case "←":
+                    const screenValue = screenInput.value;
+                    screenInput.value = screenValue.slice(0, -2)
+                    break
+                case "=":
+                    if (screenInput.value) {
+                        try {
 
-        if (e.target.innerText !== "=") {
-            if (e.target.innerText !== "x" && n === 0 && e.target.innerText !== 'x y') {
-                screenInput.value += e.target.innerText;
+                            const output = eval(screenInput.value);
+                            if (output === Infinity || output === -Infinity) {
+                                throw new Error('Error');
+                            } else {
+                                screenInput.value = output;
+                            }
+                        } catch {
+                            screenInput.value = " Invalid Value 😜😢";
+                            isError = true;
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-
-        switch (e.target.innerText) {
-            case "AC":
-                screenInput.value = "";
-                break;
-            case "←":
-                const screenValue = screenInput.value;
-                screenInput.value = screenValue.slice(0, -2)
-                // screenInput.value = screenValue.slice(0,screenValue.length-2)
-                break
-            case "=":
-                // const output = eval(screenInput.value);
-                // screenInput.value = output;
-                try {
-                    const output = eval(screenInput.value);
-                    screenInput.value = output;
-                } catch {
-                    screenInput.value = " Error 😢";
-                    n = 1;
-                }
-                break;
-            case "x":
-                e.target.innerText = "*";
-                screenInput.value += e.target.innerText;
-                e.target.innerText = "x";
-                break;
-            case 'x y':
-                console.log(e.target.innerText)
-                e.target.innerText = "**";
-                screenInput.value += e.target.innerText;
-                e.target.innerHTML = `<span>x <sup>y</sup></span>`
-
-                break
-            default:
-                break;
-        }
-
     })
+
 })
+
+
+
+
+
+
+
