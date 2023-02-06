26a8a51349fffb67e1cef527f382c82dab03bbb5
Savinder <savindersingh500@gmail.com>
2023-02-06 23:41:30 +0530

    Question 04 completed

--- DOM Assignment/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/index.js
+++ DOM Assignment/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/index.js
@@ -1,21 +1,21 @@
 let clashCards = document.getElementsByClassName('clash-card__unit-stats');
-const c = document.getElementsByClassName('clash-card__unit-stats--barbarian clearfix')[0]
 clashCards = [...clashCards];
-console.log(clashCards)
+let text = document.getElementsByClassName("one-third");
+text = [...text];
+text.forEach(text => {
+    text.style.color = "#ffffff";
+})
 clashCards.forEach(card => {
     let cardClasses = Array.from(card.classList)
-
     if (cardClasses.includes("clash-card__unit-stats--barbarian")) {
-        card.style.backgroundColor = "#ec9b3b"
+        card.style.backgroundColor = "#ec9b3b";
     } else if (cardClasses.includes("clash-card__unit-stats--archer")) {
-        card.style.backgroundColor = "#ee5487"
+        card.style.backgroundColor = "#ee5487";
     } else if (cardClasses.includes("clash-card__unit-stats--giant")) {
-        card.style.backgroundColor = "#f6901a"
+        card.style.backgroundColor = "#f6901a";
     } else if (cardClasses.includes("clash-card__unit-stats--goblin")) {
-        card.style.backgroundColor = "#82bb30"
+        card.style.backgroundColor = "#82bb30";
     } else if (cardClasses.includes("clash-card__unit-stats--wizard")) {
-        card.style.backgroundColor = "#4facff"
+        card.style.backgroundColor = "#4facff";
     }
-
-
 });
