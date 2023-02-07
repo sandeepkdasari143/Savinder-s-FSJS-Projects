ef9e1f1820c7737a2dd65dbff8c39c0e66491853
Savinder <savindersingh500@gmail.com>
2023-02-07 09:40:43 +0530

    Question 06 Solved

---
+++ DOM Assignment/DOM Assignments 2.0 Batch copy/06_DOM Project/06_DOM Project/index.js
@@ -0,0 +1,7 @@
+// Task-1
+const logo = document.querySelector("header > img")
+logo.src = "./assets/ineuron-logo.png"
+
+//Task-2
+const appPrice = document.querySelector(".app_price> span");
+appPrice.innerText = "$10";
