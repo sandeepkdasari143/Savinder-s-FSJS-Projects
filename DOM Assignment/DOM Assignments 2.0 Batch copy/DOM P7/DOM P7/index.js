ae749fd675fc260a636a733ec2b6bbf4cb60addf
Savinder <savindersingh500@gmail.com>
2023-02-11 09:46:15 +0530

    Code optimised

--- DOM Assignment/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/index.js
+++ DOM Assignment/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/index.js
@@ -1,9 +1,7 @@
 // Task 01
 
 let languages = Array.from(document.querySelectorAll(".main__languages > a"))
-console.log(languages)
 languages.forEach(elem => {
-    console.log(elem)
     if (elem.innerText.includes("2.0")) {
         elem.style.display = "none"
     }
@@ -14,31 +12,82 @@ const submitBtn = document.getElementsByClassName('main__form-btn')[0];
 submitBtn.disabled = false;
 const inputField = document.getElementsByClassName('main__form-input')[0];
 inputField.disabled = false;
-languages = Array.from(document.querySelectorAll(".main__languages > a"))
-submitBtn.addEventListener('click', () => {
-    let names = localStorage.getItem('names');
-    if (inputField.value) {
-        if (names === null) {
-            namesValue = [];
-        } else {
-            namesValue = JSON.parse(names)
-        }
-        namesValue.push(inputField.value);
-        localStorage.setItem('names', JSON.stringify(namesValue))
-    }
+let namesValue;
+const language = document.querySelector(".main__languages")
+const names = localStorage.getItem('names');
+if (names) {
+    namesValue = JSON.parse(names);
 
+} else {
+    namesValue = [];
+}
+submitBtn.addEventListener('click', () => {
+    const inputValue = inputField.value;
+    namesValue.push(inputValue)
+    localStorage.setItem('names', JSON.stringify(namesValue))
 })
 
-function append() {
+// function to add text in left card
 
-    const storedValue = JSON.parse(localStorage.getItem('names'));
-    const languagesBox = document.getElementsByClassName("main__languages")[0];
-    storedValue.forEach(name => {
-        const link = document.createElement('a');
-        link.href = "https://www.ineuron.ai";
-        link.target = "_blank"
-        link.innerText = name;
-        languagesBox.appendChild(link)
+function addText() {
+    const aTag = language.getElementsByTagName('a')[0];
+    namesValue.forEach(text => {
+        const aTagNew = aTag.cloneNode(true);
+        aTagNew.innerText = text;
+        language.appendChild(aTagNew)
+
     })
 }
-append();
+addText();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// submitBtn.addEventListener('click', () => {
+//     let names = localStorage.getItem('names');
+//     if (inputField.value) {
+//         if (names === null) {
+//             namesValue = [];
+//         } else {
+//             namesValue = JSON.parse(names)
+//         }
+//         namesValue.push(inputField.value);
+//         localStorage.setItem('names', JSON.stringify(namesValue))
+//     }
+
+// })
+
+// function append() {
+
+//     const storedValue = JSON.parse(localStorage.getItem('names'));
+//     const languagesBox = document.getElementsByClassName("main__languages")[0];
+//  if (storedValue === null) {
+//             namesValue = [];
+//         } else {
+//             namesValue = JSON.parse(names)
+//         }
+
+//     storedValue.forEach(name => {
+//         const link = document.createElement('a');
+//         link.href = "https://www.ineuron.ai";
+//         link.target = "_blank"
+//         link.innerText = name;
+//         languagesBox.appendChild(link)
+//     })
+// }
+// append();
